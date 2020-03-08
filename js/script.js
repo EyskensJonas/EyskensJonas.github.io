0e62a6ec81a49de8db8a8a9a82c460c11f0757df
EyskensJonas <59091002+EyskensJonas@users.noreply.github.com>
2020-03-08 22:30:00 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,15 @@
-// Countdown Timer
-var deadline = new Date("2020-05-18").getTime();
-var x = setInterval(function() {
-    // datum & tijd van vandaag
-    var vandaag = new Date().getTime();
-    // verschil tussen deadline & vandaag
-    var verschil = deadline - vandaag;
-    // berekening: dagen, uren, minute, seconden
-    var dagen = Math.floor(verschil / (1000 * 60 * 60 * 24));
-    var uren = Math.floor((verschil % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minuten = Math.floor((verschil % (1000 * 60 * 60)) / (1000 * 60));
-    var seconden = Math.floor((verschil % (1000 * 60)) / 1000);
-    // display de countdown
-    var result = "";
-    result += "<p>" + dagen + "</p><p class='after'>dagen</p><br>";
-    result += "<p>" + uren + "</p><p class='after'>uren</p><br>";
-    result += "<p>" + minuten + "</p><p class='after'>minuten</p><br>";
-    result += "<p>" + seconden + "</p><p class='after'>seconden</p>";
-    document.getElementById('countdown').innerHTML = result;
+// countdown
+var countdownDate = new Date("2020-05-18").getTime();
+var countdown = setInterval(function(){
+    var today = new Date().getTime();
+    var distance = countdownDate - today;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById('countdownClock').innerHTML = "<p>" + days + "<span>dagen</span>" + hours + "<span>uren</span>" + minutes + "<span>minuten</span>" + seconds + "<span>seconden</span></p>";
+    if(distance < 0) {
+        clearInterval(countdown);
+        document.getElementById('countdownClock').innerHTML = "EXPIRED";
+    }
 }, 1000);
